c6dcc7ec30e30d14ac05e9d1ced31eeaa311ed10
sagarjadhav <sagarjadhav27@gmail.com>
2013-11-01 19:41:56 +0530

    grid class

--- template-custom.php
+++ template-custom.php
@@ -8,9 +8,11 @@
  *
  * @since rtPanelChild 2.0
  */
-get_header(); ?>
+get_header();
 
-    <section id="content" role="main" class="rtp-grid-8 rtp-custom-page">
+    $rtp_content_grid_class = apply_filters( 'rtp_set_content_grid_class', 'large-8 columns' ); ?>
+
+    <section id="content" role="main" class="rtp-custom-page <?php echo $rtp_content_grid_class; ?>">
         <?php rtp_hook_begin_content(); ?>
         
         <?php
